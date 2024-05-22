c137ba29f116f899ae9c57e01af7f859ad0c561f
Ming Huang <ymhuang82@gmail.com>
2024-05-22 18:58:03 +0800

    2024/05/22

--- common.py
+++ common.py
@@ -2,6 +2,46 @@ from copy import deepcopy
 from typing import Union
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __eq__(self, other):
+        if not self and not other:
+            return True
+        if not self or not other:
+            return False
+        return (
+            self.val == other.val
+            and self.left == other.left
+            and self.right == other.right
+        )
+
+
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = [root]
+    index = 1
+    while index < len(values):
+        node = queue.pop(0)
+        if node:
+            if index < len(values) and values[index] is not None:
+                node.left = TreeNode(values[index])
+                queue.append(node.left)
+            index += 1
+
+            if index < len(values) and values[index] is not None:
+                node.right = TreeNode(values[index])
+                queue.append(node.right)
+            index += 1
+    return root
+
+
 def run_tests(
     questions: list[dict],
     answers: list,
